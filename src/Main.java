003945463712998f952af20001c446b4482e35da
Pedro <pedroivobarretogomes26@gmail.com>
2023-07-19 15:01:43 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso cursoUm = new Curso();
+        cursoUm.setTitulo("Java");
+        cursoUm.setDescricao("descrição curso java");
+        cursoUm.setCargaHoraria(8);
+
+        Curso cursoDois = new Curso();
+        cursoDois.setTitulo("JavaScript");
+        cursoDois.setDescricao("descrição curso javascript");
+        cursoDois.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(cursoUm);
+        System.out.println(cursoDois);
+        System.out.println(mentoria);
     }
 }
