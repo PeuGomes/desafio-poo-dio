2472147281fbafb9027291fe302c67227d5ba483
Pedro <pedroivobarretogomes26@gmail.com>
2023-07-19 21:19:30 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,39 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(cursoUm);
-        System.out.println(cursoDois);
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(cursoUm);
+        bootcamp.getConteudos().add(cursoDois);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro Ivo");
+        devPedro.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Pedro: " + devPedro.getConteudosInscritos());
+
+        devPedro.progredir();
+        devPedro.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Pedro: " + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Pedro: " + devPedro.getConteudosConcluidos() + " Concluído em: " +bootcamp.getDataFinal());
+        System.out.println("XP: " + devPedro.calcularTotalXp());
+
+        System.out.println("-------------------------------");
+
+        Dev devPaulo = new Dev();
+        devPaulo.setNome("Paulo Rocha");
+        devPaulo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos  Paulo:" + devPaulo.getConteudosInscritos());
+        devPaulo.progredir();
+        devPaulo.progredir();
+        devPaulo.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos  Paulo: " + devPaulo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Paulo: " + devPaulo.getConteudosConcluidos()+ " Concluído em: " +bootcamp.getDataFinal());
+        System.out.println("XP: " + devPaulo.calcularTotalXp());
+
     }
 }
